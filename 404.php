605367ac7b24c8de9b7e69bfd551064012dcc3d4
Drew Poland <1743054+bmoredrew@users.noreply.github.com>
2023-09-28 21:40:03 -0400

    underscores base

--- 404.php
+++ 404.php
@@ -1,8 +1,60 @@
-<?php 
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package lionfish
+ */
 
 get_header();
+?>
 
-esc_html_e( 'Well this is embarrassing. We seemed to have misplaced a page.' );
-get_search_form();
+	<main id="primary" class="site-main">
 
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lionfish' ); ?></h1>
+			</header><!-- .page-header -->
+
+			<div class="page-content">
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'lionfish' ); ?></p>
+
+					<?php
+					get_search_form();
+
+					the_widget( 'WP_Widget_Recent_Posts' );
+					?>
+
+					<div class="widget widget_categories">
+						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'lionfish' ); ?></h2>
+						<ul>
+							<?php
+							wp_list_categories(
+								array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								)
+							);
+							?>
+						</ul>
+					</div><!-- .widget -->
+
+					<?php
+					/* translators: %1$s: smiley */
+					$lionfish_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'lionfish' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$lionfish_archive_content" );
+
+					the_widget( 'WP_Widget_Tag_Cloud' );
+					?>
+
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
+
+	</main><!-- #main -->
+
+<?php
 get_footer();
